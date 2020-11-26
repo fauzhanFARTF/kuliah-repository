98fb0aacea5411cc60763669391d6bcaf8ed6b0a
fauzhanFARTF <fauzan1812@gmail.com>
2020-11-26 20:18:48 +0700

    Pertemuan 3 - Algoritma-Pemrograman

---
+++ Pertemuan3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+cout<<"Selamat Datang di Program C++"<<endl;
+cout<<"--------------------------"<<endl;
+cout<<"Nama Siswa : Fauzan Nurrachman"<<endl;
+cout<<"Semester : 1"<<endl;
+cout<<"Jurusan : Sistem Informasi"<<endl;
+}
