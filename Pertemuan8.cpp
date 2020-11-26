ec5b1afabab490f25f386e236213e55a40cecd2a
fauzhanFARTF <fauzan1812@gmail.com>
2020-11-26 20:43:41 +0700

    Pertemuan 8 - Algoritma-Pemrograman

---
+++ Pertemuan8.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+ 	int x, y, z;
+    cout << "==========================================\n";
+  	cout << "Program Menghitung Bilangan Terbesar\n";
+  	cout << "==========================================\n"<<endl;
+    cout << "Nilai x ? "; cin >> x;
+    cout << "Nilai y ? "; cin >> y;
+    cout << "Nilai z ? "; cin >> z;
+    if((x>y)&&(x>z))
+    cout << "Bilangan Terbesar Adalah " << x << endl;
+	else if((y>x)&&(y>z))
+    cout << "Bilangan Terbesar Adalah " << y << endl;
+    else
+    cout << "Bilangan Terbesar Adalah " << z << endl; 
+}
