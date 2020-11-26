64c3ec82899b103217701863b1c1d27a5ff377dc
fauzhanFARTF <fauzan1812@gmail.com>
2020-11-26 20:22:42 +0700

    Pertemuan 4 - Algoritma-Pemrograman

---
+++ Pertemuan4a.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x,y;
+    x=2;
+    y=3;	
+    cout<<"Nilai x = " <<x <<endl;
+    cout<<"Nilai y = " <<y <<endl;
+}
---
+++ Pertemuan4b.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int nilai_x, nilai_y;
+    cout << "Tuliskan nilai x dan y pisahkan dengan spasi: "<< endl;
+    cin >> nilai_x ;
+	cin >> nilai_y ;
+    cout << "Nilai x : " << nilai_x << endl;
+    cout << "Nilai y : " << nilai_y << endl;
+    return 0;
+}
