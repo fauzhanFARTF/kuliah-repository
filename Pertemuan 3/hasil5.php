9fcc2705ba70fb2b3126a64109603a8d90907ae9
fauzhanFARTF <fauzan1812@gmail.com>
2021-03-16 22:26:55 +0700

    Pertemuan 3 - pemrograman-berbasis-web 2-edit

--- Pertemuan 3/hasil5.php
+++ Pertemuan 3/hasil5.php
@@ -1,16 +1,15 @@
 <html>
     <body>
         <?php 
-        $conn = mysqli_connect("localhost","root","root","5bmalamdb");
-        if (mysqli_connect_errno()){
-            echo "Koneksi Gagal".mysqli_connect_error();
-        }
-            $vNIM = $_POST['NIM'];
-            $vnama =$_POST['nama'];
-            $vemail = $_POST['email'];
-            $valamat = $_POST['alamat'];
-        $query = mysqli_query($conn, "insert into mahasiswa (NIM,nama,email,alamat) values('$vNIM','$vnama','$vemail','$valamat')");
-        
+            $conn = mysqli_connect("localhost","root","root","5bmalamdb");
+            if (mysqli_connect_errno()){
+                echo "Koneksi Gagal".mysqli_connect_error();
+            }
+                $vNIM = $_POST['NIM'];
+                $vnama =$_POST['nama'];
+                $vemail = $_POST['email'];
+                $valamat = $_POST['alamat'];
+            $query = mysqli_query($conn, "insert into mahasiswa (NIM,nama,email,alamat) values('$vNIM','$vnama','$vemail','$valamat')");
         ?>
         terima kasih telah mendaftar
     </body>
